7af6882f6a619ad20204d545280449d54c11468d
Isaac <uniqucode@gmail.com>
2022-08-07 17:29:18 +0100

    Refrencing navItems

--- js/script.js
+++ js/script.js
@@ -5,3 +5,4 @@ const nav2 = document.getElementById('nav-2');
 const nav3 = document.getElementById('nav-3');
 const nav4 = document.getElementById('nav-4');
 const nav5 = document.getElementById('nav-5');
+const navItems = [nav1, nav2, nav3, nav4, nav5];
