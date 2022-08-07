1d50a4f03e8759157e44bfe5c0610d45b2f7c79f
Isaac <uniqucode@gmail.com>
2022-08-07 17:26:47 +0100

    Refrencing nav3

--- js/script.js
+++ js/script.js
@@ -2,3 +2,4 @@ const menuBars = document.getElementById('menu-bars');
 const overlay = document.getElementById('overlay');
 const nav1 = document.getElementById('nav-1');
 const nav2 = document.getElementById('nav-2');
+const nav3 = document.getElementById('nav-3');
